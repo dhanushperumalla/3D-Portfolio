abf3fd68053d0333b53e7e00d58ee1abf00e3628
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-05-10 13:23:03 +0530

    Complete Nav-Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={"Home"} />
+          <Route path="/about" element={"About"} />
+          <Route path="/projects" element={"Projects"} />
+          <Route path="/contact" element={"Contacts"} />
+        </Routes>
+      </Router>
+    </main>
+  );
 }
 
 export default App;
